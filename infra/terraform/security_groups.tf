7391029aeafb7e81b4a7c8626409f347a8a89391
Sam <samerhaj44@gmail.com>
2026-08-05 20:06:54 +0300

    Fixed Jenkins Ci/CD Deployment Pipeline

--- infra/terraform/security_groups.tf
+++ infra/terraform/security_groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "cloudcart_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = [var.my_ip,var.jenkins_ip]
   }
 
   ingress {
